2e6fc6af6fdfc1735c395e41286e86321df5a06e
James Muscat <jamesremuscat@gmail.com>
2023-08-02 19:47:17 +0100

    Try a different tack: get data from the advertisement broadcasts.

---
+++ docs/monitor.md
@@ -0,0 +1,63 @@
+# Monitoring EW-D devices
+
+The data displayed in the EW-D app in non-connected mode is found in the two
+types of BLE advertisements broadcast by the receiver. Any Bluetooth receiver
+can read this data, and no active connection to the EW-D device is necessary.
+
+In both cases, the data is contained as a binary string in the
+`manufacturer_data` section of the advertisements.
+
+## Version information
+
+The shorter of the two advertisements contains the current version numbers of
+the firmware running on the EW-D receiver and (when connected) transmitter.
+
+Total length: 6 bytes
+
+- Byte 0: Receiver major version
+- Byte 1: Receiver minor version
+- Byte 2: Receiver patch version
+- Byte 3: Transmitter major version (or `0xFF` if no transmitter connected)
+- Byte 4: Transmitter minor version (or `0xFF` if no transmitter connected)
+- Byte 5: Transmitter patch version (or `0xFF` if no transmitter connected)
+
+## Device state information
+
+The longer (and less frequent - once every couple of seconds or so) of the
+advertisements contains device state information. This is the information the
+app can show you without actively making a connection to the receiver.
+
+Total length: 18 bytes
+
+- Byte 0 - 1: Seems to always be `0x02 0x03`
+- Byte 2 - 5: ?
+- Byte 6 - 9: Frequency in kHz (unsigned 4-byte little-endian integer)
+- Byte 10: Receiver status bitmask
+  - 1: Mute lock enabled
+  - 2: Receiver panel keypad lock enabled
+  - 4: Connected to transmitter
+  - 8: AF peaking (at moment of broadcast)
+  - 16: Muted
+  - 32: Settings are being changed on the receiver panel
+  - 64: Firmware is corrupt
+  - 128: "Live TX view" (briefly true when transmitter first connects)
+- Byte 11: Transmitter device ID
+- Byte 12: Transmitter capsule type
+- Byte 13: Transmitter power (factory rating - NOT RF signal strength)
+- Byte 14: Tx battery percentage (or `0x7F` if no transmitter connected)
+- Byte 15 - 16: Tx battery runtime estimate in minutes (unsigned 2-byte le int)
+- Byte 17: Transmitter status bitmask
+  - 1: Phantom power
+  - 2: Framing mode (whatever that is)
+  - 4: Battery is rechargeable (or possibly, is the Sennheiser pack)
+  - 8: Battery status (low battery warning?)
+  - 16: AF peaking
+  - 32: Capsule is connected
+
+## Limitations
+
+Not included in this data are potentially useful things such as:
+
+- Device name (requires active connection to device)
+- RF signal level (not available?)
+- AF signal level (not available?)
--- ewd_network_bridge/ewd.py
+++ ewd_network_bridge/ewd.py
@@ -1,37 +1,109 @@
+from bleak import BleakScanner
+from dataclasses import dataclass
+
 import asyncio
-from bleak import BleakClient, BleakScanner
+import struct
 
 address = "c2:77:e6:6e:72:66"  # Pack 3's receiver
-
-watched_characteristics = [
-    30, 33, 42, 46, 48,
-    53, 55, 57, 59, 61, 64, 67, 69, 71
-]
+address = 'E1:CA:D0:2E:B6:71'  # Voc RF4
 
 
-async def get_values():
-    values_by_handle = {}
+@dataclass
+class EWDReceiver:
+    frequency: int = None
+    battery_runtime: int = None
+    battery_percentage: int = None
 
-    async with BleakClient(address, timeout=5) as client:
-        for charHandle in watched_characteristics:
-            val = await client.read_gatt_char(charHandle)
-            values_by_handle[charHandle] = val
+    mute_lock: bool = False
+    key_lock: bool = False
+    link_is_up: bool = False
+    peaking: bool = False
+    mute: bool = False
+    settings_being_changed: bool = False
+    firmware_corrupt: bool = False
+    live_tx_view: bool = False
 
-    return values_by_handle
+    tx_device_id: int = None
+    tx_capsule_type: int = None
+    tx_power: int = None
+
+    tx_phantom_power: bool = False
+    tx_framing_mode: bool = False
+    tx_battery_rechargeable: bool = False
+    tx_batt_status: bool = False
+    tx_peaking: bool = False
+    tx_capsule_connected: bool = False
+
+    tx_version: str = None
+    rx_version: str = None
+
+    def receive_monitoring_data(self, data):
+        freq = struct.unpack_from('i', data, 6)
+        self.frequency = freq[0]
+
+        status_bitmask = data[10]
+        self.mute_lock = (status_bitmask & 1) > 0
+        self.key_lock = (status_bitmask & 2) > 0
+        self.link_is_up = (status_bitmask & 4) > 0
+        self.peaking = (status_bitmask & 8) > 0
+        self.mute = (status_bitmask & 16) > 0
+        self.settings_being_changed = (status_bitmask & 32) > 0
+        self.firmware_corrupt = (status_bitmask & 64) > 0
+        self.live_tx_view = (status_bitmask & 128) > 0
+
+        tx_device = struct.unpack_from('BBB', data, 11)
+        self.tx_device_id = tx_device[0]
+        self.tx_capsule_type = tx_device[1]
+        self.tx_power = tx_device[2]
+
+        batt_level = struct.unpack_from('B', data, 14)
+        if batt_level[0] != 0x7F:
+            self.battery_percentage = batt_level[0]
+        else:
+            self.battery_percentage = None
+
+        batt_time = struct.unpack_from('H', data, 15)
+        if batt_time[0] != 0x0FFF:
+            self.battery_runtime = batt_time[0]
+        else:
+            self.battery_runtime = None
+
+        tx_status = data[17]
+        self.tx_phantom_power = (tx_status & 1) > 0
+        self.tx_framing_mode = (tx_status & 2) > 0
+        self.tx_battery_rechargeable = (tx_status & 4) > 0
+        self.tx_batt_status = (tx_status & 8) > 0
+        self.tx_peaking = (tx_status & 16) > 0
+        self.tx_capsule_connected = (tx_status & 32) > 0
+
+    def receive_version_data(self, data):
+        rx_version = struct.unpack_from('BBB', data, 0)
+        self.rx_version = '.'.join(map(lambda v: str(v), rx_version))
+        tx_version = struct.unpack_from('BBB', data, 3)
+        self.tx_version = '.'.join(map(lambda v: str(v), tx_version))
 
 
 async def main():
-    prev_values = {}
 
-    while True:
-        new_values = await get_values()
-        for handle, value in new_values.items():
-            if prev_values.get(handle) != value:
-                print(
-                    f'[{handle}] {prev_values.get(handle)} => {value}'
-                )
-        prev_values = new_values
-        await asyncio.sleep(5)
+    stop_event = asyncio.Event()
+
+    my_device = EWDReceiver()
+
+    def handle_data(device, advertising_data):
+        if device.address.upper() == address.upper():
+            mfr_data = advertising_data.manufacturer_data.get(1172)
+            if mfr_data is not None:
+                if len(mfr_data) == 18:
+                    my_device.receive_monitoring_data(mfr_data)
+                elif len(mfr_data) == 6:
+                    my_device.receive_version_data(mfr_data)
+                print(my_device)
+
+    async with BleakScanner(handle_data) as scanner:
+        ...
+        # Important! Wait for an event to trigger stop, otherwise scanner
+        # will stop immediately.
+        await stop_event.wait()
 
 
 if __name__ == '__main__':
